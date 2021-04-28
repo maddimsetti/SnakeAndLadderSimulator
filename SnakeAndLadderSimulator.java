960840151f79915e610e3eace12e3b127e2c3eb6
prudhvi <prudhvieethakota123@gmail.com>
2021-04-28 22:10:22 +0530

    Added:Welcome to The Snake And Ladder Game Played at Start Position 0

---
+++ SnakeAndLadderSimulator.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class SnakeAndLadderSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome the Player to Starts The Snake And Ladder Game at position 0");
+
+	}
+
+}
