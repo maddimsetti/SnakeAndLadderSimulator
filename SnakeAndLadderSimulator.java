b00e281f79ab055c26f6946f77aa60e01aa07958
prudhvi <prudhvieethakota123@gmail.com>
2021-04-28 23:33:40 +0530

    Added:Played the Game With 2 Players and Finally Reported Which Player Won the Game

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -9,40 +9,86 @@ public class SnakeAndLadderSimulator {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome the Player to Starts The Snake And Ladder Game at position 0");
 
-		int POSITION = 0;
-		int dieRole = 0;
+		int POSITION1 = 0;
+		int POSITION2 = 0;
 
-		while ( POSITION < 100 ) {
-		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("The Value Of Dice is " +diceNumber);
-                dieRole++;
+		while ( POSITION1 < 100 && POSITION2 < 100 ) {
+		int diceNumber1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		int diceNumber2 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("The Value Of Dice is " +diceNumber1);
+		System.out.println("The Value of Dice2 is" +diceNumber2);
 
 		int dieCheck = (int) Math.floor(Math.random() * 100) % 3;
 
 		   switch (dieCheck) {
 		      case IS_LADDER:
 		           System.out.println("The player gets Ladder");
-		           POSITION += diceNumber;
+		           POSITION1 += diceNumber1;
 		           break;
 	              case IS_SNAKE:
 			   System.out.println("the player gets Snake");
-			   POSITION -= diceNumber;
-			    if (POSITION <= 0)
-			       POSITION = 0;
+			   POSITION1 -= diceNumber1;
+			    if (POSITION1 <= 0)
+			       POSITION1 = 0;
 		           break;
 		      default:
 			   System.out.println("The player gets No Play");
 		           break;
 		   }
-		System.out.println("The Position of Player:" +POSITION);
-		System.out.println("The Count of Die Role :" +dieRole);
-		    if (POSITION > 100) {
-		        POSITION -= diceNumber;
-	                System.out.println("The Position of Player:" +POSITION);
+		System.out.println("The Position of Player:" +POSITION1);
+		    if (POSITION1 > 100) {
+		        POSITION1 -= diceNumber1;
+	                System.out.println("The Position of Player:" +POSITION1);
 	            }
+		    if (dieCheck == IS_LADDER) {
+	            diceNumber1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        	    System.out.println("The Value Of Dice2 is " +diceNumber1);
+                    POSITION1 += diceNumber1;
 	        }
-                System.out.println("The Final Position of Player:" +POSITION);
-                System.out.println("The Total number of Times Die Role :" +dieRole);
+	        if (POSITION1 > 100) {
+		    POSITION1 -= diceNumber1;
+		    System.out.println("The Position of Player:" +POSITION1);
+	        }
+
+	        int dieCheck2 = (int) Math.floor(Math.random() * 100) % 3;
+
+                          switch (dieCheck2) {
+	                      case IS_LADDER:
+		                 System.out.println("The second player gets Ladder");
+		                 POSITION2 += diceNumber2;
+		                 break;
+	                      case IS_SNAKE:
+	                         System.out.println("the second player gets Snake");
+	                         POSITION2 -= diceNumber2;
+			            if (POSITION2 <= 0)
+			               POSITION2 = 0;
+		                 break;
+	                      default:
+	                         System.out.println("The second player gets No Play");
+		                 break;
+	                      }
+                System.out.println("The Position of the Second Player:" +POSITION2);
+	          if (POSITION2 > 100) {
+                     POSITION2 -= diceNumber2;
+		     System.out.println("The Position of Player:" +POSITION2);
+	          }
+	          if (dieCheck2 == IS_LADDER) {
+		     diceNumber2 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+	             System.out.println("The Value Of Dice2 is " +diceNumber2);
+                     POSITION2 += diceNumber2;
+	          }
+	          if (POSITION2 > 100) {
+		     POSITION2 -= diceNumber2;
+	             System.out.println("The Position of Player:" +POSITION2);
+	          }
+	        }
+                System.out.println("The Final Position of the First Player:" +POSITION1);
+                System.out.println("The Final Position of the Second Player:" +POSITION2);
+
+                if (POSITION1 == 100)
+          	   System.out.println("THE FIRST PLAYER WON THE GAME");
+          	else
+                   System.out.println("THE SECOND PLAYER WON THE GAME");
 	}
 
 }
