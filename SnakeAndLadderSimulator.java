d022465ba9798045e1616dd2d850c42a949aa23e
prudhvi <prudhvieethakota123@gmail.com>
2021-04-28 22:17:20 +0530

    Added:The Player Rolled the Die To get OutPut between 1 to 6

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -6,6 +6,9 @@ public class SnakeAndLadderSimulator {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome the Player to Starts The Snake And Ladder Game at position 0");
 
+		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("The Value Of Dice is " +diceNumber);
+
 	}
 
 }
